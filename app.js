8861e70de3c89d7f0ccee05a5b66c77b5e7146fb
Anasxon Ummataliyev <aummataliy@gmail.com>
2025-09-13 23:13:43 +0500

    fix

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 div = document.querySelectorAll(".block");
 
+turn = 1
+let x = []
+let o = []
 
-div.forEach(element => {
+  div.forEach(element => {
     element.addEventListener('click', () => {
-        element.innerHTML = `<div class="x"><img src="./svg/x.png" alt=""> </div> `
+        if(turn % 2 != 0){
+            element.innerHTML = `<div class="x"><img src="./svg/x.png" alt=""> </div> `
+        
+        }
+        else{
+            element.innerHTML = `<div class="o">O</div>`
+        }
+        turn++;
     })
-});
+});  
+
 
 
